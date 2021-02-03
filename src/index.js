ed69d9cbca716e14f7211f6346e6efd4bef6b4fe
Joseph Perez <hello@josephdanielperez.com>
2021-02-03 09:55:26 -0800

    setting up reducers and configuring index and app files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import App from './components/App'
+import { rootReducer } from './reducers/rootReducer'
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
