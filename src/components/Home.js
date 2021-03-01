129ad0432007a7c8ff0850beff6bcc2e09b8be37
Joseph Perez <hello@josephdanielperez.com>
2021-02-28 16:08:30 -0800

    authentication

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,6 @@ const Home = () =>
 
     <div id='content'>
         <div id='container'>
-            <img src='https://media.giphy.com/media/Ae9RmQOtH8vmXCMlc4/giphy.gif' alt='MyFitnessApp Home Page'/>
-            <h3>FITNESS MADE SIMPLE</h3>
-
             <Registration />
         </div>
     </div>
--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Registration extends Component {
 
@@ -11,14 +12,54 @@ export default class Registration extends Component {
             password_confirmation: '',
             registrationErrors: ''
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        const {
+            username,
+            password,
+            password_confirmation
+        } = this.state;
+
+        axios.post('http://localhost:3000/registrations', {
+            user: {
+                username: username,
+                password: password,
+                password_confirmation: password_confirmation
+            }
+        },
+        { withCredentials: true }
+        )
+        .then(resp => console.log('registration response', resp))
+        .catch(error => console.log('registration error', error))
     }
 
     render() {
         return (
-            <div id='content'>
-                <div id='container'>
-                    
-                </div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Username: </label>
+                    <input type='username' name='username' value={this.state.username} onChange={this.handleChange} required /><br />
+
+                    <label>Password: </label>
+                    <input type='password' name='password' value={this.state.password} onChange={this.handleChange} required /><br />
+
+                    <label>Password Confirmation: </label>
+                    <input type='password' name='password_confirmation' value={this.state.password_confirmation} onChange={this.handleChange} required /><br />
+
+                    <button type='submit'>register</button>
+                </form>
             </div>
         )
     }
